c50bc466347419d914641ad1b8dab384cb495ff9
jmshanahan <comeraghsolutions@gmail.com>
2023-01-19 09:51:08 +1100

    Tidyied up display

--- src/components/Statbox.js
+++ src/components/Statbox.js
@@ -5,11 +5,12 @@ import classes from './Statbox.module.css';
 const Statbox = (props) => {
   const { name, stat } = props;
   return (
-    <div className={classes.boxborder}>
-        <label className={classes.label} htmlFor="name">{name}</label>
-        <input className={classes.statdisplay} id="name" type="text" value={stat} readOnly={true}/>
+    <div className={`${classes.control}`} >
+        <label  htmlFor={name}>{name}</label>
+        <input  id={name} type={"text"} value={stat} readOnly={true}/>
       </div>
   );
 };
 
 export default Statbox;
+
